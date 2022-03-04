2ad7a1dbefc63d6abd4c8e4727d7b54cb77f5572
oudshoorn <gaetan.oudshorn@gmail.com>
2022-03-04 11:09:52 +0100

    js encore

--- javascript/slide.js
+++ javascript/slide.js
@@ -2,13 +2,10 @@ var slideIndex = 0;
 plusSlides(1);
 setInterval(plusSlides, 4500,1);
 
-
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
